1f87e9732c7da1f6ef9ef7f4eb83df4525aed841
junedkaxioned <junedk@axioned.com>
2022-10-04 10:56:29 +0530

    Js Completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-/* Author: Zishan Ansari */
+var header = document.querySelector('header');
+
+window.addEventListener('scroll', stickyNav);
+
+function stickyNav() {
+  if (window.scrollY > header.offsetHeight + 150) {
+    header.classList.add('active')
+  }else {
+    header.classList.remove('active')
+  }
+}
+
